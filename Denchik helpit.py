4e50416d5da9355c2c962130b54062ebaa67430d
yaroslavlarionov <dlyasharagi39@gmail.com>
2025-10-23 21:09:19 +0200

    h/w

--- Denchik helpit.py
+++ Denchik helpit.py
@@ -1,9 +1,17 @@
-a1 = [1,2,3]
-a2 = [10,20,30]
+# a1 = [1,2,3]
+# a2 = [10,20,30]
+#
+# def yarik(a,b):
+#     return a*b
+#
+# result = list(map(yarik, a1, a2))
+# print(result)
+# 5.
+# for i in range(1,5):
+#    for j in range((4-i),-1,-1):
+#        print("*"*i+" "*j)
+#        break
 
-def yarik(a,b):
-    return a*b
-
-result = list(map(yarik, a1, a2))
-print(result)
-
+#
+st=int(input())
+print(st/100)
