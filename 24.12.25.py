1c06721695ac7b6add52a7982de1dd01ffdd09b3
yaroslavlarionov <dlyasharagi39@gmail.com>
2025-12-24 19:35:12 +0200

    s

---
+++ 24.12.25.py
@@ -0,0 +1,38 @@
+# st1 = input()
+# st2 = input()
+# st1_1 = st1.split(" ")
+# if len(st2) == len(st1_1[0]):
+#     list(st2)
+#     list(st1_1[0])
+#     if st2 == st1_1[0]:
+#         print("True")
+# else:
+#     print("False")
+
+# st = input()
+# substring = input()
+# k = 0
+# st = st.split(" ")
+# for i in st:
+#     if i == substring:
+#         k+=1
+# print(k)
+
+# def replace(st, old, new):
+#     string = ""
+#     if old not in st:
+#         return False
+#     while i < len(st)
+#     for i in range(len(st)):
+#         flag = False
+#         for j in range(len(old)):
+#             if st[i+j] != old[j]:
+#                 flag = True
+#                 break
+#         if not flag:
+#             string += new
+#         return string
+
+
+
+
